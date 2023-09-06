376d4dc5e8c0033e1180d1ec17f89dd3b7f2a353
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2023-09-06 23:46:24 +0530

    fix: Styling issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 /** @format */
 
 import React, { useState, useEffect } from "react";
-import "./App.css"; // Import your CSS file here
+import "./App.css";
 import Navbar from "./componets/Navbar";
-import jsonData from "./data.json"; // Import the JSON data
+import jsonData from "./data.json";
 
 function App() {
   const [filter, setFilter] = useState("");
@@ -59,10 +59,10 @@ function App() {
   return (
     <>
       <Navbar />
-      <div className="min-h-screen flex items-center bg-gray-900 text-white">
-        <div className="container mx-auto py-20">
+      <div className="min-h-screen bg-gray-900 text-white">
+        <div className="container mx-auto py-10 px-4 sm:px-6 lg:px-8">
           <div className="text-center my-5">
-            <div className="relative mx-auto w-3/4 mt-5">
+            <div className="relative mx-auto w-full sm:w-3/4 mt-5">
               <input
                 id="input"
                 className="px-4 py-2 w-full text-black rounded-lg border-2 border-gray-300 focus:border-blue-500 focus:outline-none focus:ring focus:ring-blue-200"
@@ -75,56 +75,58 @@ function App() {
               <i className="absolute top-2 right-2 text-gray-400 fas fa-search"></i>
             </div>
           </div>
-          <table className="table-auto w-full">
-            <thead>
-              <tr>
-                <th className="px-4 py-2">
-                  <b>Sr. No.</b>
-                </th>
-                <th className="px-4 py-2">
-                  <b>Name</b>
-                </th>
-                <th className="px-4 py-2">
-                  <b>Redemption Status</b>
-                </th>
-                <th className="px-4 py-2">
-                  <b>Course Completed</b>
-                </th>
-                <th className="px-4 py-2">
-                  <b>GenAI Completed</b>
-                </th>
-                <th className="px-4 py-2">
-                  <b>Total Completion</b>
-                </th>
-              </tr>
-            </thead>
-            <tbody id="gccp_body">
-              {data.map((d, i) => (
-                <tr key={i}>
-                  <td className="px-4 py-2">{i + 1}</td>
-                  <td className="px-4 py-2">
-                    <a
-                      href={d["Google Cloud Skills Boost Profile URL"]}
-                      target="_blank"
-                      className="text-blue-500"
-                    >
-                      {d["Student Name"]}
-                    </a>
-                  </td>
-                  <td className="px-4 py-2">
-                    {d["Redemption Status"] === "Yes" ? "✅" : "⚠️"}
-                  </td>
-                  <td className="px-4 py-2">{d["# of Courses Completed"]}</td>
-                  <td className="px-4 py-2">
-                    {d["# of GenAI Game Completed"]}
-                  </td>
-                  <td className="px-4 py-2">
-                    {d["Total Completions of both Pathways"]}
-                  </td>
+          <div className="overflow-x-auto">
+            <table className="table-auto w-full">
+              <thead>
+                <tr>
+                  <th className="px-4 py-2">
+                    <b>Sr. No.</b>
+                  </th>
+                  <th className="px-4 py-2">
+                    <b>Name</b>
+                  </th>
+                  <th className="px-4 py-2">
+                    <b>Redemption Status</b>
+                  </th>
+                  <th className="px-4 py-2">
+                    <b>Course Completed</b>
+                  </th>
+                  <th className="px-4 py-2">
+                    <b>GenAI Completed</b>
+                  </th>
+                  <th className="px-4 py-2">
+                    <b>Total Completion</b>
+                  </th>
                 </tr>
-              ))}
-            </tbody>
-          </table>
+              </thead>
+              <tbody id="gccp_body">
+                {data.map((d, i) => (
+                  <tr key={i}>
+                    <td className="px-4 py-2">{i + 1}</td>
+                    <td className="px-4 py-2">
+                      <a
+                        href={d["Google Cloud Skills Boost Profile URL"]}
+                        target="_blank"
+                        className="text-blue-500"
+                      >
+                        {d["Student Name"]}
+                      </a>
+                    </td>
+                    <td className="px-4 py-2">
+                      {d["Redemption Status"] === "Yes" ? "✅" : "⚠️"}
+                    </td>
+                    <td className="px-4 py-2">{d["# of Courses Completed"]}</td>
+                    <td className="px-4 py-2">
+                      {d["# of GenAI Game Completed"]}
+                    </td>
+                    <td className="px-4 py-2">
+                      {d["Total Completions of both Pathways"]}
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </>
